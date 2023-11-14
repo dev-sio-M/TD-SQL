3a7d66ff3b1c4ca341f8163dc6d02da5e289ac1c
mathis <138501804+dev-sio-M@users.noreply.github.com>
2023-11-14 15:20:58 +0100

    Create interrogerClubChv.sql

---
+++ TD-6-Associations-Réflexives/Cheveaux/interrogerClubChv.sql
@@ -0,0 +1,30 @@
+use clubChv;
+
+/*Nom du ou des géniteurs du cheval identifié par le numéro 33*/
+SELECT Pere.Nom AS NomPere, Mere.Nom AS NomMere
+FROM Cheveaux AS Cheval
+LEFT JOIN Cheveaux AS Pere ON Cheval.Père = Pere.Identifiant
+LEFT JOIN Cheveaux AS Mere ON Cheval.Mère = Mere.Identifiant
+WHERE Cheval.Identifiant = 33;
+
+
+/*Nom des chevaux dont le géniteur est le numéro 23*/
+SELECT Cheveaux.Nom
+FROM Cheveaux
+WHERE Cheveaux.Père = 23;
+
+
+
+/*Nom des chevaux dont on ne connait pas les deux géniteurs*/
+SELECT Nom
+FROM Cheveaux
+WHERE Père IS NULL AND Mère IS NULL;
+
+
+
+
+/*Nom de chaque cheval, et quand cela est possible, le nom de son ou de ses geniteurs*/
+SELECT Cheval.Nom AS Nom_Cheval,IFNULL(Pere.Nom, 'Inconnu') AS Nom_Pere,IFNULL(Mere.Nom, 'Inconnu') AS Nom_Mere
+FROM Cheveaux AS Cheval
+LEFT JOIN Cheveaux AS Pere ON Cheval.Père = Pere.Identifiant
+LEFT JOIN Cheveaux AS Mere ON Cheval.Mère = Mere.Identifiant;
