61cd47a8f5d4ac369ee81eadd04775a8e17243fc
mathis <138501804+dev-sio-M@users.noreply.github.com>
2023-11-14 15:15:40 +0100

    Create interrogerClubAdh.sql

---
+++ TD-6-Associations-Réflexives/Adhérents/interrogerClubAdh.sql
@@ -0,0 +1,41 @@
+use clubAdh ; 
+
+/*Nom et prénom des adhérents qui n'ont pas de parrain*/
+SELECT Nom, Prenom
+FROM Adherents
+WHERE Parrain IS NULL;
+
+/*Nombre d'adhérents parrainés dont le parrain est identifié par le numéro 2*/
+SELECT COUNT(*) AS NbAdhParraines
+FROM Adherents
+WHERE Parrain = 2;
+
+/*Nombre d'adhérents parrainés par parrain*/
+SELECT Parrain, COUNT(*) AS NbAdhParraines
+FROM Adherents
+WHERE Parrain IS NOT NULL
+GROUP BY Parrain;
+
+/*Nom et prénom du parrain le plus prolifique*/
+SELECT Nom, Prenom
+FROM Adherents
+WHERE Numero = (
+    SELECT Parrain
+    FROM Adherents
+    WHERE Parrain IS NOT NULL
+);
+
+
+/*Nom et prénom des adhérents parrainés avec pour chacun d'eux le nom et le prénom du parrain*/
+SELECT Adherents.Nom, Adherents.Prenom, Parrains.Nom AS NomParrain, Parrains.Prenom AS PrenomParrain
+FROM Adherents
+LEFT JOIN Adherents AS Parrains ON Adherents.Parrain = Parrains.Numero
+WHERE Adherents.Parrain IS NOT NULL
+ORDER BY Adherents.Nom, Adherents.Prenom;
+
+
+/*Nom et prénom de tous les adhérents avec pour chaque adhérent parrainé le nom et le prénom de son parrain*/
+SELECT Adherents.Nom, Adherents.Prenom, Parrains.Nom AS NomParrain, Parrains.Prenom AS PrenomParrain
+FROM Adherents
+LEFT JOIN Adherents AS Parrains ON Adherents.Parrain = Parrains.Numero
+ORDER BY Adherents.Nom, Adherents.Prenom;
