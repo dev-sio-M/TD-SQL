8f3c33d83de2ad8a45c6bdfc00112cf7dc39aa71
mathis <138501804+dev-sio-M@users.noreply.github.com>
2023-11-14 15:32:22 +0100

    Update interrogerClubChv.sql

--- TD-6-Associations-Réflexives/Cheveaux/interrogerClubChv.sql
+++ TD-6-Associations-Réflexives/Cheveaux/interrogerClubChv.sql
@@ -14,17 +14,14 @@ FROM Cheveaux
 WHERE Cheveaux.Père = 23;
 
 
-
 /*Nom des chevaux dont on ne connait pas les deux géniteurs*/
 SELECT Nom
 FROM Cheveaux
 WHERE Père IS NULL AND Mère IS NULL;
 
 
-
-
 /*Nom de chaque cheval, et quand cela est possible, le nom de son ou de ses geniteurs*/
-SELECT Cheval.Nom AS Nom_Cheval,IFNULL(Pere.Nom, 'Inconnu') AS Nom_Pere,IFNULL(Mere.Nom, 'Inconnu') AS Nom_Mere
+SELECT Cheval.Nom AS Nom_Cheval, Pere.Nom AS Nom_Pere,Mere.Nom AS Nom_Mere
 FROM Cheveaux AS Cheval
 LEFT JOIN Cheveaux AS Pere ON Cheval.Père = Pere.Identifiant
 LEFT JOIN Cheveaux AS Mere ON Cheval.Mère = Mere.Identifiant;
